870df8a2061ce9ff419efe3dbc76806638b67391
Muhammad Hammad Zafar <hzmalix@gmail.com>
2024-02-20 18:09:23 +0400

    fix: Upgraded dependencies and deprecated code.

--- app/src/main/java/com/hzmalix/multipartreq/MainActivity.java
+++ app/src/main/java/com/hzmalix/multipartreq/MainActivity.java
@@ -1,18 +1,21 @@
 package com.hzmalix.multipartreq;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
@@ -22,6 +25,7 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -31,13 +35,15 @@ import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
-    String url = "http://subdomain.ngrok.io/endpoint"; // TODO: Replace URL
+    String url = "https://subdomain.ngrok.io/endpoint"; // TODO: Replace URL
 
-    private final int REQUEST_CAMERA = 101, REQUEST_GALLERY = 102;
     OkHttpClient client = new OkHttpClient();
     Button btnSelect, btnUpload;
     ImageView imgView;
     TextView txtResult;
+
+    private ActivityResultLauncher<Intent> cameraLauncher;
+    private ActivityResultLauncher<String> galleryLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,88 +55,32 @@ public class MainActivity extends AppCompatActivity {
         txtResult = findViewById(R.id.txtResult);
         imgView = findViewById(R.id.imgView);
 
-        btnSelect.setOnClickListener(v -> {
-            String ACTION_CAPTURE = getString(R.string.action_capture);
-            String ACTION_CHOOSE = getString(R.string.action_choose);
-            String ACTION_CANCEL = getString(R.string.action_cancel);
-            final CharSequence[] options = {ACTION_CAPTURE, ACTION_CHOOSE, ACTION_CANCEL};
-            MaterialAlertDialogBuilder builder =
-                    new MaterialAlertDialogBuilder(MainActivity.this);
-            builder.setTitle(getString(R.string.app_name))
-                    .setCancelable(true);
-            builder.setItems(options, (dialog, item) -> {
-                if (options[item].equals(ACTION_CAPTURE)) {
-                    Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(takePicture, REQUEST_CAMERA);
-                } else if (options[item].equals(ACTION_CHOOSE)) {
-                    Intent pickPhoto = new Intent(Intent.ACTION_PICK,
-                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(pickPhoto, REQUEST_GALLERY);
-                } else {
-                    dialog.dismiss();
-                }
-            });
-            builder.create().show();
-        });
+        btnSelect.setOnClickListener(this::selectImage);
+        btnUpload.setOnClickListener(this::uploadImage);
 
-        btnUpload.setOnClickListener(v -> {
-            Bitmap bitmap = ((BitmapDrawable) imgView.getDrawable()).getBitmap();
-            bitmap = getResizedBitmap(bitmap);
-
-            byte[] bytes = bitmapToBytes(bitmap);
-            if (bytes != null) {
-                RequestBody fileBody = RequestBody.create(okhttp3.MediaType.parse("image/png"), bytes);
-
-                MultipartBody multipartBody = new MultipartBody.Builder()
-                        .setType(MultipartBody.FORM)
-                        .addFormDataPart("image", "temp_.png", fileBody).build();
-
-                Request request = new Request.Builder().url(url).post(multipartBody).build();
-                client.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onResponse(@NonNull Call call, @NonNull Response response)
-                            throws IOException {
-                        ResponseBody body = response.body();
-                        if (body != null) {
-                            String res = body.string();
-                            Log.v("RESPONSE", res);
-                        } else {
-                            Log.e("ERROR", "Failed to upload");
+        cameraLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        if (result.getData() != null && result.getData().getExtras() != null) {
+                            Bundle extras = result.getData().getExtras();
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            imgView.setImageBitmap(imageBitmap);
                         }
-                    }
 
-                    @Override
-                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                        Log.e("ERROR", "Failed to upload");
                     }
                 });
 
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        switch (requestCode) {
-            case REQUEST_CAMERA:
-            case REQUEST_GALLERY:
-                if (resultCode == RESULT_OK && data != null) {
-                    Uri selectedImage = data.getData();
-                    imgView.setImageURI(selectedImage);
-                    if (requestCode == REQUEST_CAMERA) {
-                        Bundle extras = data.getExtras();
-                        Bitmap imageBitmap = (Bitmap) extras.get("data");
-                        imgView.setImageBitmap(imageBitmap);
+        galleryLauncher = registerForActivityResult(
+                new ActivityResultContracts.GetContent(), uri -> {
+                    if (uri != null) {
+                        imgView.setImageURI(uri);
                     }
-                }
-                break;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
+                });
     }
 
     public static byte[] bitmapToBytes(Bitmap bitmap) {
-        try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
             bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos);
             return bos.toByteArray();
         } catch (Exception e) {
@@ -154,5 +104,66 @@ public class MainActivity extends AppCompatActivity {
         return Bitmap.createScaledBitmap(bitmap, width, height, true);
     }
 
+    private void selectImage(View v) {
+        String ACTION_CAPTURE = getString(R.string.action_capture);
+        String ACTION_CHOOSE = getString(R.string.action_choose);
+        String ACTION_CANCEL = getString(R.string.action_cancel);
+        final CharSequence[] options = {ACTION_CAPTURE, ACTION_CHOOSE, ACTION_CANCEL};
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(MainActivity.this);
+        builder.setTitle(getString(R.string.app_name)).setCancelable(true);
+        builder.setItems(options, (dialog, item) -> {
+            if (options[item].equals(ACTION_CAPTURE)) {
+                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+                if (takePicture.resolveActivity(getPackageManager()) != null) {
+                    cameraLauncher.launch(takePicture);
+                } else {
+                    Toast.makeText(this, "No camera app found", Toast.LENGTH_SHORT).show();
+                }
+            } else if (options[item].equals(ACTION_CHOOSE)) {
+                galleryLauncher.launch("image/*");
+            } else {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+
+    private void uploadImage(View v) {
+        Bitmap bitmap = ((BitmapDrawable) imgView.getDrawable()).getBitmap();
+        bitmap = getResizedBitmap(bitmap);
+
+        byte[] bytes = bitmapToBytes(bitmap);
+        if (bytes != null) {
+            RequestBody fileBody = RequestBody.create(bytes, MediaType.parse("image/png"));
+            MultipartBody multipartBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("image", "temp_.png", fileBody)
+                    .build();
+
+            Request request = new Request.Builder().url(url).post(multipartBody).build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response)
+                        throws IOException {
+                    ResponseBody body = response.body();
+                    if (body != null) {
+                        String res = body.string();
+                        Log.v("RESPONSE", res);
+                    } else {
+                        Log.e("ERROR", "Failed to upload");
+                    }
+                }
+
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    Log.e("ERROR", "Failed to upload");
+                    if (e.getMessage() != null) {
+                        Log.e("ERROR_MSG", e.getMessage());
+                    }
+                }
+            });
+
+        }
+    }
 }
